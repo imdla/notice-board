2bad2d9aa33dfddcd764f4f88d01e0e2719ef8db
imdla <imdla918@gmail.com>
2025-01-19 22:38:06 +0900

    [add] PostController

--- noticeboard/src/main/java/com/example/noticeboard/domain/post/PostController.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/post/PostController.java
@@ -1,4 +1,12 @@
 package com.example.noticeboard.domain.post;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
 }
