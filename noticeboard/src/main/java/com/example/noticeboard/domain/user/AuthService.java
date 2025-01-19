ec9574fc9f7d2ba30685d76d4e14cc9e6e2d8f0f
imdla <imdla918@gmail.com>
2025-01-19 20:48:17 +0900

    [add] SignupResponseDto

--- noticeboard/src/main/java/com/example/noticeboard/domain/user/AuthService.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/user/AuthService.java
@@ -1,8 +1,11 @@
 package com.example.noticeboard.domain.user;
 
 import com.example.noticeboard.domain.user.dto.SignupRequestDto;
+import com.example.noticeboard.domain.user.dto.SignupResponseDto;
+import com.example.noticeboard.domain.user.entity.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 @Getter
@@ -10,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Transactional
     public type signup(SignupRequestDto requestDto) {
@@ -21,8 +26,10 @@ public class AuthService {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
 
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
 
+        User user = requestDto.toEntity(encodedPassword);
 
-        requestDto.toEntity();
+        return SignupResponseDto.from(userRepository.save(user));
     }
 }
