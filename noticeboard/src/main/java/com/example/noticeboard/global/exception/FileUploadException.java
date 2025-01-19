0c2ce772619ebf955718a46b22e1fc15cebbc299
imdla <imdla918@gmail.com>
2025-01-19 18:16:28 +0900

    [add] FileUploadException

---
+++ noticeboard/src/main/java/com/example/noticeboard/global/exception/FileUploadException.java
@@ -0,0 +1,12 @@
+package com.example.noticeboard.global.exception;
+
+public class FileUploadException extends RuntimeException {
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException() {
+        super("파일 업로드를 실패했습니다.");
+    }
+}
