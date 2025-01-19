f1b62d2f5918cdc84bf324cab92b23f94df3c0ac
imdla <imdla918@gmail.com>
2025-01-19 19:35:12 +0900

    [add] TagController

--- noticeboard/src/main/java/com/example/noticeboard/domain/tag/TagController.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/tag/TagController.java
@@ -1,4 +1,12 @@
 package com.example.noticeboard.domain.tag;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tag")
+@RequiredArgsConstructor
 public class TagController {
+    private TagService tagService;
 }
