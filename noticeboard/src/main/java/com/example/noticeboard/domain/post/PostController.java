cd1cad4e314e7474c254034ae6271b14d72351c4
imdla <imdla918@gmail.com>
2025-01-19 22:37:14 +0900

    [add] PostService

---
+++ noticeboard/src/main/java/com/example/noticeboard/domain/post/PostController.java
@@ -0,0 +1,4 @@
+package com.example.noticeboard.domain.post;
+
+public class PostController {
+}
---
+++ noticeboard/src/main/java/com/example/noticeboard/domain/post/PostService.java
@@ -0,0 +1,12 @@
+package com.example.noticeboard.domain.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+}
