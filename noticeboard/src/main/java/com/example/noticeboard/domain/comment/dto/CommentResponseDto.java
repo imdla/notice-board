4a96552d58052d076dcdd47a59b714a5f67e81ab
imdla <imdla918@gmail.com>
2025-01-19 22:22:03 +0900

    [add] CommentResponseDto

--- noticeboard/src/main/java/com/example/noticeboard/domain/comment/dto/CommentResponseDto.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/comment/dto/CommentResponseDto.java
@@ -1,4 +1,25 @@
 package com.example.noticeboard.domain.comment.dto;
 
+import com.example.noticeboard.domain.comment.Comment;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class CommentResponseDto {
+    private final Long id;
+    private final String content;
+    private LocalDateTime createdAt;
+    private String author;
+
+    public static CommentResponseDto from(Comment entity) {
+        return CommentResponseDto.builder()
+                .id(entity.getId())
+                .content(entity.getContent())
+                .createdAt(entity.getCreatedAt())
+                .author(entity.getAuthor().getUsername())
+                .build();
+    }
 }
