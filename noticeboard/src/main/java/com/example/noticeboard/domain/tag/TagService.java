de11753bd3858ab0a5f2dcd992fd577a121b0c46
imdla <imdla918@gmail.com>
2025-01-19 19:34:17 +0900

    [add] TagService

--- noticeboard/src/main/java/com/example/noticeboard/domain/tag/TagService.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/tag/TagService.java
@@ -1,4 +1,12 @@
 package com.example.noticeboard.domain.tag;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TagService {
+    private TagRepository tagRepository;
 }
