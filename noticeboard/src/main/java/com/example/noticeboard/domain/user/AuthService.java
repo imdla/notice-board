06ea155312d68be708d5f2947bdb09ba0111bd38
imdla <imdla918@gmail.com>
2025-01-19 19:50:29 +0900

    [add] AuthService

--- noticeboard/src/main/java/com/example/noticeboard/domain/user/AuthService.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/user/AuthService.java
@@ -1,4 +1,12 @@
 package com.example.noticeboard.domain.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Getter
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
 }
