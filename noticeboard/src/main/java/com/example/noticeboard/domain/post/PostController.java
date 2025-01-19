61bd6e6695ae45f0ba464cd677776dd860fc74e4
imdla <imdla918@gmail.com>
2025-01-19 22:42:20 +0900

    [feat] PostController addPost 기능 추가

--- noticeboard/src/main/java/com/example/noticeboard/domain/post/PostController.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/post/PostController.java
@@ -1,12 +1,31 @@
 package com.example.noticeboard.domain.post;
 
+import com.example.noticeboard.domain.user.entity.User;
+import com.example.noticeboard.global.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @PostMapping
+    public type addPost(
+            @Valid @RequestPart(value = "data") PostRequestDto requestDto,
+            @RequestPart(value = "image", required = false) MultipartFile image,
+            @AuthenticationPrincipal User user
+            ) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.ok(
+                        postService.addPost(requestDto, image, user)
+                ));
+    }
 }
