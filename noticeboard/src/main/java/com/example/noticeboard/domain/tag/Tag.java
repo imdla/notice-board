e4d00539881e1fa447847d2097c8af97033ffaec
imdla <imdla918@gmail.com>
2025-01-19 18:37:21 +0900

    [add] Tag

---
+++ noticeboard/src/main/java/com/example/noticeboard/domain/tag/Tag.java
@@ -0,0 +1,18 @@
+package com.example.noticeboard.domain.tag;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    private String name;
+}
