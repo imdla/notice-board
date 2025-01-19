96a45fd71ea160903ffd3fabe1af4cff81d8f086
imdla <imdla918@gmail.com>
2025-01-19 22:08:33 +0900

    [feat] addComment 기능 추가

--- noticeboard/src/main/java/com/example/noticeboard/domain/comment/CommentController.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/comment/CommentController.java
@@ -1,12 +1,27 @@
 package com.example.noticeboard.domain.comment;
 
+import com.example.noticeboard.global.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts/{postId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @PostMapping
+    public type addComment(
+            @PathVariable Long postId,
+            @Valid @RequestBody CommentRequestDto requestDto
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.ok(
+                        commentService.addComment(postId, requestDto)
+                ));
+    }
 }
