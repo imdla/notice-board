74d83e3375147ff45da81331f33ac31fcb2feb16
imdla <imdla918@gmail.com>
2025-01-19 22:05:31 +0900

    [add] CommentService

--- noticeboard/src/main/java/com/example/noticeboard/domain/comment/CommentService.java
+++ noticeboard/src/main/java/com/example/noticeboard/domain/comment/CommentService.java
@@ -1,4 +1,12 @@
 package com.example.noticeboard.domain.comment;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
 }
