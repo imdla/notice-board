28fde10524f45ae6adaba366251a206296df04e6
imdla <imdla918@gmail.com>
2025-01-19 18:13:43 +0900

    [add] ResourceNotFoundException

---
+++ noticeboard/src/main/java/com/example/noticeboard/global/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.noticeboard.global.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        super("리소스를 찾을 수 없습니다.");
+    }
+}
